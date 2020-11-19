e962f45a2c4615decc74ef9fbb803fc60d3b8009
heeju <heeju0world@hotmail.com>
2020-11-20 00:08:02 +0900

    getting the location

--- App.js
+++ App.js
@@ -1,21 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import Loading from "./Loading";
+import * as Location from "expo-location";
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+export default class extends React.Component {
+  getLocation = async() => {
+      const location = await Location.getCurrentPositionAsync(option)
+  }
+  componentDidMount(){
+    
+  }
+  render() {
+    return <Loading />;
+  }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
